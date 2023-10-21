65359a7ae8b70204cc0f8db2fbc53d137156ef93
sakshig10 <2017.sakshi.gujarathi@ves.ac.in>
2023-10-21 02:34:36 -0400

    forecasting completed

--- Task 1/main.py
+++ Task 1/main.py
@@ -42,3 +42,59 @@ CONNECTION_PARAMETERS = {
 }
 
 session = Session.builder.configs(CONNECTION_PARAMETERS).create()
+session.sql('USE WAREHOUSE AD_FORECAST_DEMO_WH').collect()
+
+st.title("Orginal Dataframe with daily impressions and counts")
+original_dataframe=session.sql('select * from daily_impressions;').collect()
+st.write(original_dataframe)
+
+
+session.sql('CALL impressions_forecast!FORECAST(FORECASTING_PERIODS => 14)').collect()
+query = '''
+SELECT day AS ts, impression_count AS actual, NULL AS forecast, NULL AS lower_bound, NULL AS upper_bound 
+FROM daily_impressions 
+UNION ALL 
+SELECT ts, NULL AS actual, forecast, lower_bound, upper_bound 
+FROM TABLE(RESULT_SCAN(-1))
+'''
+result = session.sql(query)
+
+
+
+
+pandas_dataframe = result.toPandas()
+st.title("Dataframe")
+st.dataframe(pandas_dataframe)
+
+
+
+
+
+
+# Function to create the time series graph
+def create_time_series_plot(data):
+    print(data)
+    df = data
+    df['TS'] = pd.to_datetime(df['TS'])
+    df.set_index('TS', inplace=True)
+
+    fig, ax = plt.subplots(figsize=(12, 6))
+    ax.plot(df.index, df['ACTUAL'], label='Actual', color='blue')
+    ax.plot(df.index, df['FORECAST'], label='Forecast', color='red')
+    ax.fill_between(df.index, df['LOWER_BOUND'], df['UPPER_BOUND'], color='gray', alpha=0.5, label='Prediction Interval')
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Value')
+    ax.set_title('Time Series Graph')
+    ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d"))
+    ax.xaxis.set_major_locator(mdates.DayLocator(interval=10))
+    plt.gcf().autofmt_xdate()
+    ax.legend()
+    ax.grid(True)
+    
+    return fig
+
+# Streamlit app
+st.title("Time Series Data")
+
+# Display the plot in Streamlit
+st.pyplot(create_time_series_plot(pandas_dataframe))
