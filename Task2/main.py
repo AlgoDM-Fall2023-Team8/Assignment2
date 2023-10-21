5d09788d042e800f36061db624ed767b1902c3f9
Ansh <anshmiteshchhadva@gmail.com>
2023-10-21 07:33:48 -0400

    Completed the final multipage app and  updated the Readme

---
+++ Task2/main.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Streamlit! 👋")
+
+st.sidebar.success("STREAM FLAKE ❤️‍🔥")
+
+st.markdown(
+    """
+# Assignment2  ❤️‍🔥
+## Welcome to the multipage app!
+### The setup
+  * Create a conda environment using the provided environment.yml file.
+    - `conda env create -f environment.yml`
+  * Activate that created conda environment by `conda activate pysnowpark_ml_tpcds`.
+  * Edit the creds.json file with your account information to connect to your account.
+  * Load Jupyter or equivalent notebook to begin executing the notebook.
+
+  [Code Lab Link](https://docs.google.com/document/d/1tR45WPhxRbq6_FXf4MKTivlmEEWpQe0XEKOBG449s_0/edit?usp=sharing)
+"""
+)
